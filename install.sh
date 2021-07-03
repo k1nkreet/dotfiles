9427fa262a1177c5a4fb727b9ea631a169c34fba
k1nkreet <polyakovskiy.ilya@gmail.com>
2021-07-03 12:50:10 +0300

    add mutt configuration to install script

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ warn() {
 type stow || die "GNU stow should be installed"
 type vim && stow vim || warn "vim is not found, skipping vim configs"
 type tmux && stow tmux || warn "tmux is not found, skipping tmux configs"
+type mutt && stow mutt || warn "mutt is not found, skipping mutt configs"
 stow xinit
 stow tools
 stow bash
